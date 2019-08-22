73fed7f479dd43714dcafc12880fe3c96132ad8b
hillarydalie <hidalie@gmail.com>
2019-08-22 10:26:04 +0300

    Change the pipe code

--- src/app/sema-datecount.pipe.ts
+++ src/app/sema-datecount.pipe.ts
@@ -5,8 +5,22 @@ import { Pipe, PipeTransform } from '@angular/core';
 })
 export class SemaDatecountPipe implements PipeTransform {
 
-  transform(value: any, ...args: any[]): any {
+  transform(value: any): number {
+
+      let today:Date = new Date(); 
+      let todayWithoutTime:any = new Date(today.getFullYear(), today.getMonth(), today.getDate())
+      var dateDifference = Math.abs(value - todayWithoutTime) 
+      const secondsInDay = 86400; 
+      var dateDifferenceSeconds = dateDifference*0.001;
+      var dateCounter = dateDifferenceSeconds/secondsInDay;
+
+
+      if (dateCounter >= 1 ){
+        return dateCounter;
+      }else{
+        return 0;
+      }
+
     return null;
   }
-
 }
