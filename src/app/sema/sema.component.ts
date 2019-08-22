9d51a77c818cf83904858903d07f3ef34030097b
hillarydalie <hidalie@gmail.com>
2019-08-22 10:11:10 +0300

    Add styling to the like and dislike button

--- src/app/sema/sema.component.ts
+++ src/app/sema/sema.component.ts
@@ -12,10 +12,7 @@ export class SemaComponent implements OnInit {
    quotes:Quote [] = [
     new Quote(1,'John Piper', 'Hillary Dalie', 'God is most glorified in us when we are most satisfied in Him',new Date(2012,3,14),0,0),
     new Quote(2,'J.I. Packer','Daniel Kamata', 'Your faith will not fail while God purposes to sustain it.',new Date(2013,4,1),0,0),
-    new Quote(3,'Mwaura Muthee', 'Kip Otele','Not everytime change has to be given, sometimes change has to be asked for',new Date(2013,8,14),0,0),
-    new Quote(4,'Arsene Wenger', 'Njoki Munanie','Young players need freedom of expression to develop as creative players.',new Date(2015,7,6),0,0),
-    new Quote(5,'Josep Guardiola','Mephibosheth David','I will forgive the player if the players cannot get it right, but not if they do not try hard.',new Date(2016,3,1),0,0),
-    new Quote(6,'Hillary Dalie','John Musembi','Be a child again, dip your cookie in milk, eat candy, dance. Then wake up and live everyday.',new Date(2017,7,1),0,0),
+    new Quote(3,'Mwaura Muthee', 'Kip Otele','Not everytime change has to be given, sometimes change has to be asked for.',new Date(2013,8,14),0,0),
   ];
 
   addNewQuote(quote) {
